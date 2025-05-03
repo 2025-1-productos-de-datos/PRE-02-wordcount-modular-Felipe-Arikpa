3e49ebb28ae669d8897fbe7fd219fd74f52c7f48
Luis Felipe Aricapa Muñoz <laricapa@unal.edu.co>
2025-05-03 10:14:46 -0500

    mover funciona internals

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,30 +1,20 @@
 import os
 
+from ._internals.write_count_words import write_count_words
 
 def main():
-    input_files_list = os.listdir('data/input/')
+    input_files_list = os.listdir("data/input/")
 
-    counter={}
+    counter = {}
     for filename in input_files_list:
-        with open('data/input/'+filename) as f:
+        with open("data/input/" + filename) as f:
             for l in f:
-                for w in l.split( ):
+                for w in l.split():
                     w = w.lower().strip(",.!?")
                     counter[w] = counter.get(w, 0) + 1
 
-
     write_count_words(counter)
 
-def write_count_words(counter):
-    if not os.path.exists('data/output'):
-        os.makedirs('data/output')
 
-    # save the results using tsv format
-    with open("data/output/results.tsv", "w", encoding="utf-8") as f:
-            for key, value in counter.items():
-                # write the key and value to the file
-                f.write(f"{key}\t{value}\n")
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
